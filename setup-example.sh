4ab19c692b6b1234f66a161b892591a26f43ae01
stefzke <spfluege@gmail.com>
2014-11-18 09:31:02 +0100

    accidentially overwrote setup-example.sh file. changed it back.

--- setup-example.sh
+++ setup-example.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-export COMPWA_DIR=$PWD
+export COMPWA_DIR=/Users/weidenka/work/rootAnalysis/ComPWA
 export PWA_LIBS=$COMPWA_DIR/lib
-export EXTERNAL_DIR=/home/pflueger/ComPWA_externals
+export EXTERNAL_DIR=/Users/weidenka/work/external
 
 export QFT=$EXTERNAL_DIR/qft++
-export GENEVA=$EXTERNAL_DIR/geneva_1_4_0
-export MINUIT2=$EXTERNAL_DIR/Minuit2-5.34.14
+export GENEVA=
+export MINUIT2=$EXTERNAL_DIR/Minuit2-5.28.00/build
 #BOOST installation root directory
-#export BOOST_ROOT=/home/weidenka/external/boost_1_54_0/build
-export BOOST_ROOT=$EXTERNAL_DIR/boost_1_55_0
+export BOOST_ROOT=$EXTERNAL_DIR/boost_1_54_0/build
 #BOOST.build root directory (!)
-export BOOST_BUILD_PATH=$BOOST_ROOT/share/boost-build
-export ROOTSYS=$EXTERNAL_DIR/root_v5.34.23
+export BOOST_BUILD_PATH=$BOOST_ROOT/../tools/build/v2
+export ROOTSYS=$EXTERNAL_DIR/root-v5.34.09
 
-export PATH=$BOOST_ROOT/bin:$ROOTSYS/bin:$PATH
-export LD_LIBRARY_PATH=$GENEVA/lib:$ROOTSYS/lib/root:$MINUIT2/lib:$BOOST_ROOT/lib:$PWA_LIBS:$QFT/lib:$LD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/weidenka/work/rootAnalysis/DKsKK/lib
-#export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GENEVA/lib:$ROOTSYS/lib/root:$MINUIT2/lib:$BOOST_ROOT/lib:$PWA_LIBS:$QFT/lib
-#export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/weidenka/work/rootAnalysis/DKsKK/lib
+export PATH=$PATH:$BOOST_ROOT/bin:$ROOTSYS/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GENEVA/lib:$ROOTSYS/lib/root:$MINUIT2/lib:$BOOST_ROOT/lib:$PWA_LIBS:$QFT/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/weidenka/work/rootAnalysis/DKsKK/lib
+export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GENEVA/lib:$ROOTSYS/lib/root:$MINUIT2/lib:$BOOST_ROOT/lib:$PWA_LIBS:$QFT/lib
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/weidenka/work/rootAnalysis/DKsKK/lib
 
 #echo "check libs:"
 #echo $LD_LIBRARY_PATH
