52efdeaacf7edc7a6d7790d68c769e37bad492b0
spflueger <spfluege@gmail.com>
2015-06-29 10:58:44 +0200

    created blank amplitude tree factorys that will create the amplitude trees base on helicity decay trees

---
+++ Physics/HelicityAmplitude/HelicityAmplitudeTreeFactory.cpp
@@ -0,0 +1,14 @@
+#include "HelicityAmplitudeTreeFactory.hpp"
+
+namespace HelicityFormalism {
+
+  HelicityAmplitudeTreeFactory::HelicityAmplitudeTreeFactory() {
+    // TODO Auto-generated constructor stub
+
+  }
+
+  HelicityAmplitudeTreeFactory::~HelicityAmplitudeTreeFactory() {
+    // TODO Auto-generated destructor stub
+  }
+
+} /* namespace HelicityFormalism */
---
+++ Physics/HelicityAmplitude/HelicityAmplitudeTreeFactory.hpp
@@ -0,0 +1,14 @@
+#ifndef PHYSICS_HELICITYAMPLITUDE_HELICITYAMPLITUDETREEFACTORY_HPP_
+#define PHYSICS_HELICITYAMPLITUDE_HELICITYAMPLITUDETREEFACTORY_HPP_
+
+namespace HelicityFormalism {
+
+  class HelicityAmplitudeTreeFactory {
+  public:
+    HelicityAmplitudeTreeFactory();
+    virtual ~HelicityAmplitudeTreeFactory();
+  };
+
+} /* namespace HelicityFormalism */
+
+#endif /* PHYSICS_HELICITYAMPLITUDE_HELICITYAMPLITUDETREEFACTORY_HPP_ */
