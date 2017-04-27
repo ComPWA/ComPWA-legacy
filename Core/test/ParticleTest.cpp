e51947787da17d2e05e9f186b55d6732f549cb3e
Peter Weidenkaff <weidenka@uni-mainz.de>
2017-04-27 17:26:56 +0800

    Adding test cases for FourMomentum and Particle classes

---
+++ Core/test/ParticleTest.cpp
@@ -0,0 +1,39 @@
+
+#define BOOST_TEST_MODULE Core
+
+#include <memory>
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+#include <Core/Exceptions.hpp>
+#include <Core/Particle.hpp>
+
+namespace ComPWA {
+
+BOOST_AUTO_TEST_SUITE(ParticleTest);
+
+BOOST_AUTO_TEST_CASE(FourMomentum) {
+  ComPWA::FourMomentum p4(1,2,3,4);
+  ComPWA::FourMomentum p4B(1,2,3,4);
+  BOOST_CHECK_EQUAL(p4, p4B);
+  BOOST_CHECK_EQUAL(p4.GetMassSq(), 2.0);
+  BOOST_CHECK_EQUAL(ComPWA::FourMomentum::ThreeMomentumSq(p4), 14.0);
+  
+  p4B.SetFourMomentum( std::array<double,4>{{1,2,3,5}} );
+  auto pTot = p4B+p4; //(2,4,6,9)
+  BOOST_CHECK_EQUAL(pTot.GetMassSq(), 25.0);
+  
+  ComPWA::FourMomentum pSum;
+  pSum += p4;
+  pSum += p4B;
+  BOOST_CHECK_EQUAL(pSum.GetMassSq(), 25.0);
+}
+  
+BOOST_AUTO_TEST_CASE(Particle) {
+  ComPWA::Particle part(1,2,3,4);
+  BOOST_CHECK_EQUAL(part.GetMassSq(), 2.0);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
+} /* namespace ComPWA */
