0036b8ff8c78673d0ecf0a738190e46bfbb378ac
Peter Weidenkaff <weidenka@uni-mainz.de>
2017-08-08 11:49:25 +0800

    Fixing some compiler warnings

--- Core/Parameter.hpp
+++ Core/Parameter.hpp
@@ -1241,7 +1241,7 @@ BOOST_SERIALIZATION_SHARED_PTR(ComPWA::DoubleParameter)
  @param pt Input property tree
  @return Parameter
  */
-static DoubleParameter
+inline DoubleParameter
 DoubleParameterFactory(const boost::property_tree::ptree pt) {
   DoubleParameter obj;
 
