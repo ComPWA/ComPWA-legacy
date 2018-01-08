8737f7e68ffd7aaafba15bccf38c5d802a7c5781
Mathias Michel <michel@kph.uni-mainz.de>
2018-01-08 10:12:30 +0100

    update python example

--- Examples/PythonDalitzFit/dalitzfit.py
+++ Examples/PythonDalitzFit/dalitzfit.py
@@ -21,110 +21,106 @@ from histogrammar import *
 ############################# Configuration ##############
 
 amplitudeModel = '''
-<IncoherentIntensity Name="jpsiGammaPiPi_inc">
-	<CoherentIntensity Name="jpsiGammaPiPi">
-  	<Amplitude Name="f2(1270)">
-			<Parameter Type="Magnitude"	Name="Magnitude_f2">
-				<Value>1.0</Value>
+<Intensity Class='Incoherent' Name="jpsiGammaPiPi_inc">
+  <Intensity Class='Coherent' Name="jpsiGammaPiPi">
+    <Amplitude Class="SequentialPartialAmplitude" Name="f2(1270)">
+      <Parameter Class='Double' Type="Magnitude"  Name="Magnitude_f2">
+        <Value>1.0</Value>
         <Min>-1.0</Min>
         <Max>2.0</Max>
-				<Fix>false</Fix>
-			</Parameter>
-			<Parameter Type="Phase" Name="Phase_f2">
-				<Value>0.0</Value>
+        <Fix>false</Fix>
+      </Parameter>
+      <Parameter Class='Double' Type="Phase" Name="Phase_f2">
+        <Value>0.0</Value>
         <Min>-100</Min>
         <Max>100</Max>
-				<Fix>false</Fix>
-			</Parameter>
-			<Resonance Name="f2ToPiPi">
-				<DecayParticle Name="f2(1270)" Helicity="0"/>
-				<SubSystem>
-					<RecoilSystem FinalState="0" />
-					<DecayProducts>
-						<Particle Name="pi0" FinalState="1"  Helicity="0"/>
-						<Particle Name="pi0" FinalState="2"  Helicity="0"/>
-					</DecayProducts>
-				</SubSystem>
-			</Resonance>
-		</Amplitude>
-		<Amplitude Name="myAmp">
-			<Parameter Type="Magnitude"	Name="Magnitude_my">
-				<Value>1.0</Value>
+        <Fix>false</Fix>
+      </Parameter>
+      <PartialAmplitude Class="HelicityDecay" Name="f2ToPiPi">
+        <DecayParticle Name="f2(1270)" Helicity="0"/>
+        <RecoilSystem FinalState="0" />
+        <DecayProducts>
+          <Particle Name="pi0" FinalState="1"  Helicity="0"/>
+          <Particle Name="pi0" FinalState="2"  Helicity="0"/>
+        </DecayProducts>
+      </PartialAmplitude>
+    </Amplitude>
+    <Amplitude Class="SequentialPartialAmplitude" Name="myAmp">
+      <Parameter Class='Double' Type="Magnitude"  Name="Magnitude_my">
+        <Value>1.0</Value>
         <Min>-1.0</Min>
         <Max>2.0</Max>
-				<Fix>true</Fix>
-			</Parameter>
-			<Parameter Type="Phase" Name="Phase_my`">
-				<Value>0.0</Value>
+        <Fix>true</Fix>
+      </Parameter>
+      <Parameter Class='Double' Type="Phase" Name="Phase_my`">
+        <Value>0.0</Value>
         <Min>-100</Min>
         <Max>100</Max>
-				<Fix>true</Fix>
-			</Parameter>
-			<Resonance Name="MyResToPiPi">
-				<DecayParticle Name="myRes" Helicity="0"/>
-				<SubSystem>
-					<RecoilSystem FinalState="0" />
-					<DecayProducts>
-						<Particle Name="pi0" FinalState="1"  Helicity="0"/>
-						<Particle Name="pi0" FinalState="2"  Helicity="0"/>
-					</DecayProducts>
-				</SubSystem>
-			</Resonance>
-		</Amplitude>
-	</CoherentIntensity>
-</IncoherentIntensity>
+        <Fix>true</Fix>
+      </Parameter>
+      <PartialAmplitude Class="HelicityDecay" Name="MyResToPiPi">
+        <DecayParticle Name="myRes" Helicity="0"/>
+        <RecoilSystem FinalState="0" />
+        <DecayProducts>
+          <Particle Name="pi0" FinalState="1"  Helicity="0"/>
+          <Particle Name="pi0" FinalState="2"  Helicity="0"/>
+        </DecayProducts>
+      </PartialAmplitude>
+    </Amplitude>
+  </Intensity>
+</Intensity>
 '''
 
 myParticles = '''
 <ParticleList>
-	<Particle Name="f2(1270)">
-		<Pid>225</Pid>
-		<Parameter Type="Mass" Name="Mass_f2(1270)">
-			<Value>1.2755</Value>
-			<Error>8.0E-04</Error>
+  <Particle Name="f2(1270)">
+    <Pid>225</Pid>
+    <Parameter Class='Double' Type="Mass" Name="Mass_f2(1270)">
+      <Value>1.2755</Value>
+      <Error>8.0E-04</Error>
       <Min>0.1</Min>
       <Max>2.0</Max>
       <Fix>false</Fix>
-		</Parameter>
-		<QuantumNumber Class="Spin" Type="Spin" Value="2"/>
-		<QuantumNumber Class="Int" Type="Charge" Value="0"/>
-		<QuantumNumber Class="Int" Type="Parity" Value="+1"/>
-		<QuantumNumber Class="Int" Type="Cparity" Value="+1"/>
-		<DecayInfo Type="relativisticBreitWigner">
-			<FormFactor Type="0" />
-			<Parameter Type="Width" Name="Width_f2(1270)">
-				<Value>0.1867</Value>
-			</Parameter>
-			<Parameter Type="MesonRadius" Name="Radius_rho">
-				<Value>2.5</Value>
-				<Fix>true</Fix>
-			</Parameter>
-		</DecayInfo>
-	</Particle>
-	<Particle Name="myRes">
-		<Pid>999999</Pid>
-		<Parameter Type="Mass" Name="Mass_myRes">
-			<Value>2.0</Value>
-			<Error>8.0E-04</Error>
-		</Parameter>
-		<QuantumNumber Class="Spin" Type="Spin" Value="1"/>
-		<QuantumNumber Class="Int" Type="Charge" Value="0"/>
-		<QuantumNumber Class="Int" Type="Parity" Value="+1"/>
-		<QuantumNumber Class="Int" Type="Cparity" Value="+1"/>
-		<DecayInfo Type="relativisticBreitWigner">
-			<FormFactor Type="0" />
-			<Parameter Type="Width" Name="Width_myRes">
-				<Value>1.0</Value>
+    </Parameter>
+    <QuantumNumber Class="Spin" Type="Spin" Value="2"/>
+    <QuantumNumber Class="Int" Type="Charge" Value="0"/>
+    <QuantumNumber Class="Int" Type="Parity" Value="+1"/>
+    <QuantumNumber Class="Int" Type="Cparity" Value="+1"/>
+    <DecayInfo Type="relativisticBreitWigner">
+      <FormFactor Type="0" />
+      <Parameter Class='Double' Type="Width" Name="Width_f2(1270)">
+        <Value>0.1867</Value>
+      </Parameter>
+      <Parameter Class='Double' Type="MesonRadius" Name="Radius_rho">
+        <Value>2.5</Value>
+        <Fix>true</Fix>
+      </Parameter>
+    </DecayInfo>
+  </Particle>
+  <Particle Name="myRes">
+    <Pid>999999</Pid>
+    <Parameter Class='Double' Type="Mass" Name="Mass_myRes">
+      <Value>2.0</Value>
+      <Error>8.0E-04</Error>
+    </Parameter>
+    <QuantumNumber Class="Spin" Type="Spin" Value="1"/>
+    <QuantumNumber Class="Int" Type="Charge" Value="0"/>
+    <QuantumNumber Class="Int" Type="Parity" Value="+1"/>
+    <QuantumNumber Class="Int" Type="Cparity" Value="+1"/>
+    <DecayInfo Type="relativisticBreitWigner">
+      <FormFactor Type="0" />
+      <Parameter Class='Double' Type="Width" Name="Width_myRes">
+        <Value>1.0</Value>
         <Min>0.1</Min>
         <Max>1.0</Max>
         <Fix>false</Fix>
-			</Parameter>
-			<Parameter Type="MesonRadius" Name="Radius_myRes">
-				<Value>2.5</Value>
-				<Fix>true</Fix>
-			</Parameter>
-		</DecayInfo>
-	</Particle>
+      </Parameter>
+      <Parameter Class='Double' Type="MesonRadius" Name="Radius_myRes">
+        <Value>2.5</Value>
+        <Fix>true</Fix>
+      </Parameter>
+    </DecayInfo>
+  </Particle>
 </ParticleList>
 '''
 
@@ -165,7 +161,6 @@ print("     Fit model to data")
 fitPar = pwa.ParameterList()
 intens.GetParameters(fitPar)
 pwa.setErrorOnParameterList(fitPar, 0.05, False)
-print(fitPar.ToString())
 
 esti = pwa.MinLogLH(kin, intens, sample, phspSample, phspSample, 0, 0)
 esti.UseFunctionTree(True)
