8f3e9b8dc64966a756f9200461e7d2c96c9eb1d0
spflueger <spfluege@gmail.com>
2018-08-14 16:56:46 +0200

    - cleaned up plotting module code slightly - weighted phsp data is now reweighted to automatically have matching   integrals with data sample

--- Examples/JPsiToPi+Pi-Pi0/plot.py
+++ Examples/JPsiToPi+Pi-Pi0/plot.py
@@ -1,16 +1,13 @@
-from Plotting.plot import make_dalitz_plot, make_angular_distribution_plot
-from Plotting.ROOT.rootplotdatareader import open_compwa_plot_data, load_root
-
+from Plotting.plot import make_dalitz_plot, make_comparison_plot_1d
+from Plotting.ROOT.rootplotdatareader import open_compwa_plot_data
 
 plot_data = open_compwa_plot_data("plot.root")
 
-invariant_masses = plot_data.data.filter(regex='mSq')
+data_variables = list(plot_data.data.columns.values)
+print("found data variables:",data_variables)
+plot_variables = []
+for x in data_variables:
+    if 'mSq' in x or 'cosTheta' in x or 'phi' in x:
+        plot_variables.append(x)
 
-#print(invariant_masses[[invariant_masses.columns[0]]])
-#print(invariant_masses[[invariant_masses.columns[1]]])
-#make_dalitz_plot(invariant_masses[[invariant_masses.columns[0]]], invariant_masses[[invariant_masses.columns[1]]])
-
-make_angular_distribution_plot(plot_data.data[['cosTheta_34_2']], alpha=0.7, bins=40)
-make_angular_distribution_plot(plot_data.data[['phi_34_2']], alpha=0.7, bins=40)
-make_angular_distribution_plot(plot_data.data[['cosTheta_3_4_vs_2']], alpha=0.7, bins=40)
-make_angular_distribution_plot(plot_data.data[['phi_3_4_vs_2']], alpha=0.7, bins=40)
+make_comparison_plot_1d(plot_data, plot_variables, alpha=0.7, bins=40)
