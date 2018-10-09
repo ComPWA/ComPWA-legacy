f3a4aa97c3167b32ab5bb62741726917ff8a4a66
Stefan Pflueger <spfluege@gmail.com>
2018-10-09 18:02:57 +0200

    - Fixed evaluation of Wigner D functions in helicity formalism   Only the small Wigner d function were previously evaluated!   Also improved calculation speed by using theta instead of cosTheta!

--- Examples/JPsiToPi+Pi-Pi0/plot.py
+++ Examples/JPsiToPi+Pi-Pi0/plot.py
@@ -4,12 +4,12 @@ from Plotting.ROOT.rootplotdatareader import open_compwa_plot_data
 plot_data = open_compwa_plot_data("plot.root")
 
 data_variables = list(plot_data.data.columns.values)
-print("found data variables:",data_variables)
+print("found data variables:", data_variables)
 plot_variables = []
 for x in data_variables:
-    if 'mSq' in x or 'cosTheta' in x or 'phi' in x:
+    if 'mSq' in x or 'theta' in x or 'phi' in x:
         plot_variables.append(x)
 
 make_comparison_plot_1d(plot_data, plot_variables, alpha=0.7, bins=40)
 
-make_dalitz_plots(plot_data, alpha=0.7, bins=[50,50])
+make_dalitz_plots(plot_data, alpha=0.7, bins=[50, 50])
