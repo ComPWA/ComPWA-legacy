91394e125c43803dadd7e004a0125e5277fd9968
Stefan Pflueger <spfluege@gmail.com>
2018-11-05 12:03:45 +0100

    - Switched to uproot for ROOT IO from python (removed rootnumpy).   This means ROOT with python bindings is not required anymore.

--- Tools/Plotting/ROOT/rootplotdatareader.py
+++ Tools/Plotting/ROOT/rootplotdatareader.py
@@ -1,49 +1,38 @@
+import uproot
+
+
 def open_compwa_plot_data(input_file_path):
-    from ROOT import gROOT, TFile, gDirectory
     from Plotting.plot import PlotData
     pd = PlotData()
 
-    gROOT.Reset()
     # open file
-    file = TFile(input_file_path, 'READ')
-    file.cd("final_state_id_to_name_mapping")
-    for k in gDirectory.GetListOfKeys():
-        pd.particle_id_to_name_mapping[k.ReadObj(
-        ).GetVal()] = k.ReadObj().GetName()
+    file = uproot.open(input_file_path)
+    trees = file.keys()
 
-    from root_numpy import list_trees
-    trees = list_trees(input_file_path)
-    if "tree_data" in trees:
+    file = file.get("final_state_id_to_name_mapping")
+    for k, v in file.items():
+        pd.particle_id_to_name_mapping[v] = k.decode()[:k.decode().find(';')]
+
+    if "tree_data" in [x.decode()[:x.decode().find(';')] for x in trees]:
         pd.data = load_ttree(input_file_path, "tree_data")
-    if "tree_weighted_phsp_MC" in trees:
+    if "tree_weighted_phsp_MC" in [x.decode()[:x.decode().find(';')]
+                                   for x in trees]:
         pd.fit_result_data = load_ttree(
             input_file_path, "tree_weighted_phsp_MC")
 
     return pd
 
 
-def load_ttree(fname, tree=None, patterns=None, *kargs, **kwargs):
+def load_ttree(filename, treename, branchnames=None):
     """
     Loads a root ttree into a numpy record array
-    Further *kargs and *kwargs are passed to root_numpy's root2array.
-    >>> df = load_ttree('test.root', 'MyTree', patterns=['x_*', 'y_*'], 
-                         selection='x_1 > 100')
+    If branchnames is None all branches are read.
     """
-    from root_numpy import root2array, list_trees
     import numpy as np
+    tree = uproot.open(filename)[treename]
 
-    if tree is None:
-        branches = list_trees(fname)
-        if len(branches) == 1:
-            tree = branches[0]
-        else:
-            raise ValueError('More than one tree found in {}'.format(fname))
-
-    if not patterns:
-        all_vars = None
-    else:
-        # TODO: select only specifiy branches based on the patterns
-        all_vars = None
-
-    arr = root2array(fname, tree, all_vars, *kargs, **kwargs)
-    return np.rec.array(arr)
+    if not branchnames:
+        branchnames = tree.keys()
+    array_dict = tree.arrays(branchnames)
+    return np.rec.fromarrays(array_dict.values(),
+                             names=[x.decode() for x in array_dict.keys()])
