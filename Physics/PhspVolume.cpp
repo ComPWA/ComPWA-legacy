68855e519711804158dbc39e35e1d8f203b51669
Remco de Boer <remco@ihep.ac.cn>
2019-10-29 11:22:53 +0100

    Removed MC integration + cleaned up interface (fixes #37)

--- Physics/PhspVolume.cpp
+++ Physics/PhspVolume.cpp
@@ -4,37 +4,61 @@
 
 #include "Physics/PhspVolume.hpp"
 #include "Core/Exceptions.hpp"
-#include "Core/Random.hpp"
-#include "Data/DataSet.hpp"
-#include "Tools/Integration.hpp"
 #include <algorithm>
 #include <cmath>
-#include <iostream>
 #include <numeric>
 
 namespace ComPWA {
 namespace Physics {
 
-std::pair<double, double>
-phspVolumeRiemann(double s, std::vector<double> &masses, size_t nsteps) {
+class IntegrationSample : public std::vector<double> {
+public:
+  IntegrationSample(double lower, double upper, std::size_t SampleSize = 100);
+  const double BinSize;
+};
+
+IntegrationSample::IntegrationSample(double lower, double upper, std::size_t nvalues)
+    : BinSize{(upper - lower) / (nvalues - 1)} {
+  this->resize(nvalues);
+  double bin_size = BinSize;
+  double start_value = lower - BinSize;
+  std::generate(begin(), end(),
+                [&start_value, &bin_size] { return start_value += bin_size; });
+}
+
+std::pair<double, double> SRange(double s, std::vector<double> &masses) {
+  std::pair<double, double> s_range = {0., std::sqrt(s)};
+  // * Compute s lower
+  for (std::size_t i = 0; i < masses.size() - 1; ++i)
+    s_range.first += masses[i];
+  s_range.first *= s_range.first; // squared
+  // * Compute s upper
+  s_range.second -= masses.back();
+  s_range.second *= s_range.second; // squared
+  return s_range;
+}
+
+std::pair<double, double> PhspVolume(double s, std::vector<double> &masses,
+                                     std::size_t SampleSize) {
   if (masses.size() == 2)
-    return std::make_pair(PhspTwoParticles(s, masses[0], masses[1]), 0.);
+    return std::make_pair(PhspVolumeTwoParticles(s, masses[0], masses[1]), 0.);
   else {
     if (masses.size() > 2) {
       // * Create integration sample
-      /// @TODO: Results in many integration samples when computing 5 particles
       auto s_range = SRange(s, masses);
       auto sample =
-          IntegrationSample(s_range.first, s_range.second, nsteps + 1);
+          IntegrationSample(s_range.first, s_range.second, SampleSize + 1);
       // * Create profile vector of phasespace for N - 1 particles
+      /// @TODO: Better to reuse this sample per recursion. Now it is
+      /// regenerated for each iteration.
       std::vector<double> previousPhsp(sample.size() - 1);
       auto masses_new = masses;
       masses_new.pop_back();
       double mNsq = masses.back() * masses.back();
-      for (size_t i = 1; i < sample.size(); ++i) { // don't include s' = 0
-        previousPhsp[i - 1] = SqrtKallenFunction(s, sample[i], mNsq);
+      for (std::size_t i = 1; i < sample.size(); ++i) { // don't include s' = 0
+        previousPhsp[i - 1] = std::sqrt(KallenFunction(s, sample[i], mNsq));
         previousPhsp[i - 1] *=
-            phspVolumeRiemann(sample[i], masses_new, nsteps).first;
+            PhspVolume(sample[i], masses_new, SampleSize).first;
         previousPhsp[i - 1] *= sample.BinSize;
       }
       // * Integrate that profile vector
@@ -49,127 +73,16 @@ phspVolumeRiemann(double s, std::vector<double> &masses, size_t nsteps) {
   }
 }
 
-double PhspTwoParticles(double s, double m1, double m2) {
-  return (2 * M_PI) * ThresholdFunction(s, m1, m2);
-}
-
-double ThresholdFunction(double s, double m1, double m2) {
-  return SqrtKallenFunction(s, m1 * m1, m2 * m2) / s;
-}
-
-double SqrtKallenFunction(double x, double y, double z) {
-  double result = KallenFunction(x, y, z);
-  if (result < 0.)
-    return 0.;
-  else
-    return std::sqrt(result);
+double PhspVolumeTwoParticles(double s, double m1, double m2) {
+  return (2 * M_PI) * std::sqrt(KallenFunction(s, m1 * m1, m2 * m2)) / s;
 }
 
 double KallenFunction(double x, double y, double z) {
-  return x * x + y * y + z * z - 2 * x * y - 2 * y * z - 2 * z * x;
-}
-
-std::pair<double, double> SRange(double s, std::vector<double> &masses) {
-  std::pair<double, double> s_range = {0., std::sqrt(s)};
-  // * Compute s lower
-  for (size_t i = 0; i < masses.size() - 1; ++i)
-    s_range.first += masses[i];
-  s_range.first *= s_range.first; // squared
-  // * Compute s upper
-  s_range.second -= masses.back();
-  s_range.second *= s_range.second; // squared
-  return s_range;
-}
-
-/// Generate a vector of size \p nvalues of equally spaced values lying between
-/// and including \p lower and \p upper. The bin size is stored as a data
-/// member, because it is used repeatedly when computing a Rieman integral.
-/// @TODO: Move to general tools and make into a template. Note that you'll run
-/// into trouble at `generate`.
-IntegrationSample::IntegrationSample(double lower, double upper, size_t nvalues)
-    : BinSize{(upper - lower) / (nvalues - 1)} {
-  this->resize(nvalues);
-  double bin_size = BinSize;
-  double start_value = lower - BinSize;
-  std::generate(begin(), end(),
-                [&start_value, &bin_size] { return start_value += bin_size; });
-}
-
-std::pair<double, double> phspVolumeMC(double s, std::vector<double> FSMasses,
-                                       size_t NumEvaluations) {
-  PhaseSpaceVolume PSVolumeFunction(s, FSMasses);
-
-  // define the intervals (identical to the definition above)
-  std::vector<std::pair<double, double>> SIntervals;
-  auto FSMassBegin = FSMasses.begin() + 2;
-  auto FSMassEnd = FSMasses.end();
-  double upper(s);
-  double TempMassSum(std::accumulate(FSMasses.begin(), FSMasses.end(), 0.0));
-  for (auto x = FSMassBegin; x != FSMassEnd; ++x) {
-    upper = std::pow(std::sqrt(upper) - *x, 2);
-    TempMassSum -= *x;
-    SIntervals.push_back(std::make_pair(std::pow(TempMassSum, 2), upper));
-  }
-  std::reverse(std::begin(SIntervals), std::end(SIntervals));
-
-  // generate "points" uniform in the s intervals
-  ComPWA::Data::DataSet Data;
-  StdUniformRealGenerator Random(1234);
-  std::vector<double> point(SIntervals.size());
-  for (size_t i = 0; i < NumEvaluations; ++i) {
-    std::transform(SIntervals.begin(), SIntervals.end(), point.begin(),
-                   [&Random](const std::pair<double, double> &limits) {
-                     double r = Random();
-                     return limits.first + r * (limits.second - limits.first);
-                   });
-    Data.Data.push_back(point);
-    Data.Weights.push_back(1.0);
-  }
-
-  double HyperCubeVolume(1.0);
-  for (auto const &x : SIntervals) {
-    HyperCubeVolume *= (x.second - x.first);
-  }
-
-  // then just call the numerical integration
-  return ComPWA::Tools::integrateWithError(PSVolumeFunction, Data,
-                                           HyperCubeVolume);
-}
-
-std::vector<double> PhaseSpaceVolume::evaluate(
-    const std::vector<std::vector<double>> &points) noexcept {
-  std::vector<double> results;
-  for (auto point : points) {
-    double result(0.0);
-    if (point.size() > 0) {
-      result = KallenFunction(ISMass, point.back(), FSMassesSquared.back());
-      for (size_t i = 1; i < point.size(); ++i) {
-        if (point[i] < point[i - 1]) {
-          result = 0.0;
-          break;
-        }
-        result *=
-            KallenFunction(point[i], point[i - 1], FSMassesSquared[i + 1]);
-      }
-      result *=
-          KallenFunction(point.front(), FSMassesSquared[0], FSMassesSquared[1]);
-      if (result < 0) {
-        result = 0.0;
-      }
-    } else {
-      result = KallenFunction(ISMass, FSMassesSquared[0], FSMassesSquared[1]);
-    }
-
-    if (result > 0.0) {
-      result = 2.0 * M_PI / ISMass * std::sqrt(result);
-      for (auto const &x : point) {
-        result *= M_PI / x;
-      }
-    }
-
-    results.push_back(result);
-  }
-  return results;
+  double result = x * x + y * y + z * z - 2 * x * y - 2 * y * z - 2 * z * x;
+  if (result < 0.)
+    return 0.;
+  else
+    return result;
 }
 
 } // namespace Physics
--- Physics/PhspVolume.hpp
+++ Physics/PhspVolume.hpp
@@ -11,31 +11,22 @@
 #ifndef PhspVolume_h
 #define PhspVolume_h
 
-#include "Core/Function.hpp"
+#include <utility>
 #include <vector>
 
 namespace ComPWA {
 namespace Physics {
 
 /// Compute phasespace volume of momentum space for an arbitrary number of
-/// particles in the final state *using Monte Carlo integration*.
-std::pair<double, double> phspVolumeMC(double s, std::vector<double> FSMasses,
-                                       size_t NumEvaluations = 100000);
-
-/// Compute phasespace volume of momentum space for an arbitrary number of
-/// particles in the final state *using Riemann integration*.
+/// particles in the final state using Riemann integration.
+/// @return A pair: first value is the volume, second is the error (currently
+/// set to `0.`)
+/// @TODO Implement errors (second member of the pair).
 /// @TODO Algorithm might be improved with [Simpson's
 /// rule](https://en.wikipedia.org/wiki/Simpson%27s_rule), because we integrate
 /// over a function that is polynomial in the limit \f$m_i\rightarrow 0\f$
-std::pair<double, double> phspVolumeRiemann(double s,
-                                            std::vector<double> &FSMasses,
-                                            size_t NumEvaluations = 1000);
-
-class IntegrationSample : public std::vector<double> {
-public:
-  IntegrationSample(double lower, double upper, size_t nsteps = 100);
-  const double BinSize;
-};
+std::pair<double, double> PhspVolume(double s, std::vector<double> &FSMasses,
+                                     std::size_t SampleSize = 1000);
 
 /// Original [Källén
 /// function](https://en.wikipedia.org/wiki/K%C3%A4ll%C3%A9n_function), that is,
@@ -44,37 +35,10 @@ public:
 /// formula](https://en.wikipedia.org/wiki/Heron%27s_formula)), because we need
 /// to enter \f$s\f$ without taking its square root.
 double KallenFunction(double x, double y, double z);
-double SqrtKallenFunction(double x, double y, double z);
-
-/// Threshold function. Can be interpreted as the particle velocity at a given
-/// center-of-mass energy \p s in case of equal masses (\p m1 = \p m2).
-/// Vanishes below threshold.
-double ThresholdFunction(double s, double m1, double m2);
 
 /// Phase space element for a two particle decay. An analytic solution exists
 /// only for the volume of the phasespace of two-particle decays.
-double PhspTwoParticles(double s, double m1, double m2);
-
-std::pair<double, double> SRange(double s, std::vector<double> &masses);
-
-class PhaseSpaceVolume : public Intensity {
-public:
-  PhaseSpaceVolume(double ISMass_, std::vector<double> FSMasses_)
-      : ISMass(ISMass_), FSMassesSquared(FSMasses_) {
-    for (auto &x : FSMassesSquared)
-      x = x * x;
-  }
-
-  std::vector<double>
-  evaluate(const std::vector<std::vector<double>> &points) noexcept;
-
-  void updateParametersFrom(const std::vector<double> &){};
-  std::vector<Parameter> getParameters() const { return {}; };
-
-private:
-  double ISMass;
-  std::vector<double> FSMassesSquared;
-};
+double PhspVolumeTwoParticles(double s, double m1, double m2);
 
 } // namespace Physics
 } // namespace ComPWA
