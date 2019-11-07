c634c7752462f5a7ef148f041f89a10d192a0b30
spflueger <spfluege@gmail.com>
2019-11-07 14:06:12 +0100

    Core tests cleanup (#250)
    * Removed redundant and inconsistent CX0TestApp.
      The TestApp did not use the boost unit test framework and the tests are
      superfluous since all compilers support this by now.
    * Moved Core test cmake calls to its subdirectory
    * Activated and fixed PropertiesTest in Core module

--- Core/test/PropertiesTest.cpp
+++ Core/test/PropertiesTest.cpp
@@ -2,16 +2,16 @@
 // This file is part of the ComPWA framework, check
 // https://github.com/ComPWA/ComPWA/license.txt for details.
 
-// This can only be define once within the same library ?!
-#define BOOST_TEST_MODULE                                                      \
-  Core 
-
-#include <boost/test/unit_test.hpp>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
+#define BOOST_TEST_MODULE Core
 
 #include "Core/Properties.hpp"
 #include "Core/Logging.hpp"
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <sstream>
 
 BOOST_AUTO_TEST_SUITE(Core)
 
@@ -64,89 +64,21 @@ BOOST_AUTO_TEST_CASE(XMLInput) {
   </Particle> \
   </ParticleList>";
 
-  boost::property_tree::ptree tr;
-  boost::property_tree::xml_parser::read_xml(XMLIn, tr);
+  auto partL = ComPWA::readParticles(XMLIn);
 
-  auto partL = std::make_shared<ComPWA::PartList>();
-  ReadParticles(partL, tr);
+  auto part = ComPWA::findParticle(partL, "gamma");
+  BOOST_CHECK_EQUAL(part.getMass().Value, 0.);
+  BOOST_CHECK_EQUAL(part.getQuantumNumber<double>("Spin"), 1.);
+  BOOST_CHECK_EQUAL(part.getQuantumNumber<int>("Parity"), -1);
+  BOOST_CHECK_EQUAL(part.getQuantumNumber<int>("Cparity"), -1);
+  BOOST_CHECK_EQUAL(part.getDecayType(), "Stable");
 
-  auto part = partL->find("gamma")->second;
-  BOOST_CHECK_EQUAL(part.GetMass(), 0.);
-  BOOST_CHECK_EQUAL((double)part.GetSpinQuantumNumber("Spin"), 1.);
-  BOOST_CHECK_EQUAL(part.GetQuantumNumber("Parity"), -1);
-  BOOST_CHECK_EQUAL(part.GetQuantumNumber("Cparity"), -1);
-  BOOST_CHECK_EQUAL(part.GetDecayType(), "Stable");
-
-  part = FindParticle(partL, 9010221);
-  BOOST_CHECK_EQUAL(part.GetMass(), 0.99);
-  BOOST_CHECK_EQUAL((double)part.GetSpinQuantumNumber("Spin"), 0.);
-  BOOST_CHECK_EQUAL(part.GetQuantumNumber("Parity"), 1);
-  BOOST_CHECK_EQUAL(part.GetQuantumNumber("Cparity"), 1);
-  BOOST_CHECK_EQUAL(part.GetDecayType(), "relativisticBreitWigner");
-};
-
-BOOST_AUTO_TEST_CASE(XMLOutput) {
-  //Do not instantiate twice
-  //  ComPWA::Logging log("", boost::log::trivial::severity_level::trace);
-
-  std::stringstream XMLIn;
-  XMLIn << "<ParticleList> \
-  <Particle Name='gamma'> \
-  <Pid>22</Pid> \
-  <Parameter Type='Mass' Name='mass_gamma'> \
-  <Value>0.</Value> \
-  </Parameter> \
-  <QuantumNumber Class='Spin' Type='Spin' Value='1.0'/> \
-  <QuantumNumber Class='Int' Type='Charge' Value='0'/> \
-  <QuantumNumber Class='Int' Type='Parity' Value='-1'/> \
-  <QuantumNumber Class='Int' Type='Cparity' Value='-1'/> \
-  <QuantumNumber Class='Int' Type='Gparity' Value='-1'/> \
-  </Particle> \
-  <Particle Name='f0_980'> \
-  <Pid>9010221</Pid> \
-  <Parameter Type='Mass' Name='mass_f0_980'> \
-  <Value>0.99</Value> \
-  <Fix>1</Fix> \
-  <Min>0.5</Min> \
-  <Max>1.5</Max> \
-  <Error>0</Error> \
-  </Parameter> \
-  <QuantumNumber Class='Spin' Type='Spin' Value='0.0'/> \
-  <QuantumNumber Class='Int' Type='Charge' Value='0'/> \
-  <QuantumNumber Class='Int' Type='Parity' Value='1'/> \
-  <QuantumNumber Class='Int' Type='Cparity' Value='1'/> \
-  <QuantumNumber Class='Int' Type='Gparity' Value='1'/> \
-  <DecayInfo Type='relativisticBreitWigner'> \
-  <Parameter Type='Width' Name='width_f0_980'> \
-  <Value>0.05</Value> \
-  <Fix>1</Fix> \
-  <Min>0.025</Min> \
-  <Max>.5</Max> \
-  <Error>0</Error> \
-  </Parameter> \
-  <Parameter Type='MesonRadius' Name='radius_f0_980'> \
-  <Value>1.5</Value> \
-  <Fix>1</Fix> \
-  <Min>1.0</Min> \
-  <Max>2.0</Max> \
-  <Error>0</Error> \
-  </Parameter> \
-  </DecayInfo> \
-  </Particle> \
-  </ParticleList>";
-  
-  boost::property_tree::ptree tr;
-  boost::property_tree::xml_parser::read_xml(XMLIn, tr);
-
-  auto partL = std::make_shared<ComPWA::PartList>();
-  ReadParticles(partL, tr);
-  auto ptout = SaveParticles(partL);
-  
-  LOG(INFO) << "Writing particle list...";
-  boost::property_tree::xml_parser::write_xml("Properties-output.xml", ptout,
-                                              std::locale());
-
-  //  std::remove("Properties-output.xml"); // delete file
+  part = ComPWA::findParticle(partL, 9010221);
+  BOOST_CHECK_EQUAL(part.getMass().Value, 0.99);
+  BOOST_CHECK_EQUAL(part.getQuantumNumber<double>("Spin"), 0.);
+  BOOST_CHECK_EQUAL(part.getQuantumNumber<int>("Parity"), 1);
+  BOOST_CHECK_EQUAL(part.getQuantumNumber<int>("Cparity"), 1);
+  BOOST_CHECK_EQUAL(part.getDecayType(), "relativisticBreitWigner");
 };
 
 BOOST_AUTO_TEST_SUITE_END()
