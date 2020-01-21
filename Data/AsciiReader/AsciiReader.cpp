e113c56d5446d17261b837f7c2056ea621c64684
Remco de Boer <remco@ihep.ac.cn>
2020-01-21 08:44:05 +0100

    Upgraded AsciiReader

--- Data/AsciiReader/AsciiReader.cpp
+++ Data/AsciiReader/AsciiReader.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2013 The ComPWA Team.
 // This file is part of the ComPWA framework, check
+// Copyright (c) 2013 The ComPWA Team.
 // https://github.com/ComPWA/ComPWA/license.txt for details.
 
 #include <fstream>
@@ -8,46 +8,167 @@
 
 #include "AsciiReader.hpp"
 #include "Core/Exceptions.hpp"
+#include "Core/Logging.hpp"
 
 namespace ComPWA {
 namespace Data {
+namespace Ascii {
 
-// Constructors and destructors
-AsciiReader::AsciiReader(unsigned int NumberOfParticles_)
-    : NumberOfParticles(NumberOfParticles_) {}
-
-AsciiReader::~AsciiReader() {}
-
-std::shared_ptr<std::vector<ComPWA::Event>>
-AsciiReader::readData(const std::string &InputFilePath) const {
-
-  std::ifstream currentStream;
-  currentStream.open(InputFilePath);
-
-  if (!currentStream)
-    throw ComPWA::BadConfig("Can not open " + InputFilePath);
-
-  std::vector<ComPWA::Event> Events;
-
-  while (!currentStream.eof()) {
-    double e, px, py, pz;
-    int pid;
-    ComPWA::Event newEvent;
-
-    for (unsigned int ipart = 0; ipart < NumberOfParticles; ++ipart) {
-      currentStream >> px >> py >> pz >> e >> pid;
-      newEvent.ParticleList.push_back(ComPWA::Particle(px, py, pz, e, pid));
-    }
-
-    if (!currentStream.fail()) {
-      Events.push_back(newEvent);
-      // for ( parts = 0; parts < linesToSkip; parts++ )
-      //   currentStream >> px >> py >> pz >> e;
-    }
-  }
-  currentStream.close();
-  return std::make_shared<std::vector<ComPWA::Event>>(Events);
+std::string tolower(std::string s) {
+  std::transform(s.begin(), s.end(), s.begin(),
+                 [](unsigned char c) { return std::tolower(c); });
+  return s;
 }
 
+bool isHeaderLine(std::string line) {
+  return (tolower(line).find("header") != std::string::npos);
+}
+
+bool isEmptyString(const std::string &line) {
+  return std::all_of(line.begin(), line.end(),
+                     [](char c) { return std::isspace(c); });
+}
+
+std::ifstream openStream(const std::string &Filename) {
+  std::ifstream InputStream(Filename);
+  if (!InputStream.good())
+    throw ComPWA::CorruptFile("Cannot open file " + Filename);
+  return InputStream;
+}
+
+std::vector<int> extractHeader(std::ifstream &InputStream) {
+  std::string line;
+  // Find first header keyword
+  while (std::getline(InputStream, line))
+    if (isHeaderLine(line))
+      break;
+  // Import PIDs
+  std::vector<int> PIDs;
+  while (std::getline(InputStream, line)) {
+    if (isHeaderLine(line))
+      break;
+    std::stringstream StringStream(line);
+    std::string Word;
+    int PID;
+    if (StringStream >> Word >> PID) {
+      Word = tolower(Word);
+      if (Word.find("pid") != std::string::npos)
+        PIDs.push_back(PID);
+    }
+  }
+  return PIDs;
+}
+
+std::vector<int> extractHeader(const std::string &Filename) {
+  auto InputStream = openStream(Filename);
+  return extractHeader(InputStream);
+}
+
+std::vector<ComPWA::Event> readData(const std::string &InputFilePath,
+                                    long long NumberEventsToRead) {
+  /// -# Open file
+  auto InputStream = openStream(InputFilePath);
+
+  /// -# Extract header
+  auto PIDs = extractHeader(InputStream);
+  auto NumberOfParticles = PIDs.size();
+  if (!PIDs.size())
+    throw ComPWA::BadConfig("Input data file " + InputFilePath +
+                            " misses a header with PIDs");
+
+  /// -# Determine whether weights or not
+  auto Position = InputStream.tellg();
+  bool HasWeights = true;
+  double weight, px, py, pz, e;
+  std::string line;
+  while (std::getline(InputStream, line)) {
+    if (isEmptyString(line))
+      continue;
+    std::stringstream StringStream(line);
+    if ((StringStream >> weight) && !(StringStream >> py))
+      HasWeights = true;
+    break;
+  }
+  InputStream.seekg(Position);
+
+  /// -# Determine E,p or p,E
+  bool IsOrderEnergyMomentum = false;
+  if (HasWeights)
+    InputStream >> weight;
+  InputStream >> e >> px >> py >> pz;
+  if (e * e - px * px - py * py - pz * pz > 0.)
+    IsOrderEnergyMomentum = true;
+  InputStream.seekg(Position);
+
+  /// -# Import events
+  std::vector<ComPWA::Event> Events;
+  weight = 1.;
+  while (InputStream.good()) {
+    if (HasWeights)
+      InputStream >> weight;
+    std::vector<Particle> ParticleList;
+    ParticleList.reserve(NumberOfParticles);
+    for (size_t i = 0; i < NumberOfParticles; ++i) {
+      if (IsOrderEnergyMomentum)
+        InputStream >> e >> px >> py >> pz;
+      else
+        InputStream >> px >> py >> pz >> e;
+      ParticleList.push_back(ComPWA::Particle(px, py, pz, e, PIDs[i]));
+    }
+    if (!InputStream.fail())
+      Events.push_back({ParticleList, weight});
+    if (NumberEventsToRead > 0 &&
+        Events.size() == (std::size_t)NumberEventsToRead)
+      break;
+  }
+  InputStream.close();
+  return Events;
+}
+
+void writeData(const std::vector<ComPWA::Event> &Events,
+               const std::string &OutputFilePath, bool AppendToFile) {
+  if (!Events.size())
+    throw ComPWA::BadParameter("Cannot write empty event vector");
+  ComPWA::Logging log("warning");
+
+  /// -# Determine stream mode
+  auto OpenMode = std::ofstream::out;
+  if (AppendToFile) {
+    auto PIDs = extractHeader(OutputFilePath);
+    if (!PIDs.size()) {
+      LOG(WARNING) << "Will overwrite corrupt output file " << OutputFilePath
+                   << std::endl;
+      AppendToFile = false;
+    } else
+      OpenMode |= std::ofstream::app;
+  }
+
+  /// -# Open file
+  std::ofstream OutputStream;
+  OutputStream.open(OutputFilePath, OpenMode);
+  if (!OutputStream)
+    throw ComPWA::BadConfig("Cannot open " + OutputFilePath);
+
+  /// -# Write header
+  if (!AppendToFile) {
+    OutputStream << "Header" << std::endl;
+    for (auto Particle : Events[0].ParticleList)
+      OutputStream << "\tPid: " << Particle.pid() << std::endl;
+    OutputStream << "Header" << std::endl << std::endl;
+  }
+
+  /// -# Write events
+  for (const auto &Event : Events) {
+    OutputStream << Event.Weight << std::endl;
+    for (const auto &Particle : Event.ParticleList) {
+      OutputStream << Particle.fourMomentum().px() << "\t";
+      OutputStream << Particle.fourMomentum().py() << "\t";
+      OutputStream << Particle.fourMomentum().pz() << "\t";
+      OutputStream << Particle.fourMomentum().e() << std::endl;
+    }
+  }
+} // namespace Ascii
+
+} // namespace Ascii
 } // namespace Data
 } // namespace ComPWA
---
+++ Data/AsciiReader/test/AsciiReaderTest.cpp
@@ -0,0 +1,79 @@
+// Copyright (c) 2013, 2017 The ComPWA Team.
+// This file is part of the ComPWA framework, check
+// https://github.com/ComPWA/ComPWA/license.txt for details.
+
+#define BOOST_TEST_MODULE Data_AsciiReaderTest
+
+#include "Data/AsciiReader/AsciiReader.hpp"
+#include "Core/Logging.hpp"
+#include "Data/EvtGen/EvtGenGenerator.hpp"
+#include "Data/Generate.hpp"
+
+#include <boost/test/unit_test.hpp>
+#include <string>
+
+#include <fstream>
+
+namespace ComPWA {
+namespace Data {
+namespace Ascii {
+
+std::vector<ComPWA::Event>
+generateSample(std::size_t NumberOfEvents,
+               const ComPWA::FourMomentum &CMSP4 = 1.85,
+               const std::vector<double> &masses = {.5, .5, .5}) {
+  auto EventGenerator = ComPWA::Data::EvtGen::EvtGenGenerator(CMSP4, masses);
+  ComPWA::StdUniformRealGenerator RandomGenerator(1234);
+  std::vector<ComPWA::Event> Events(NumberOfEvents);
+  for (auto &Event : Events) {
+    Event = EventGenerator.generate(RandomGenerator);
+    Event.Weight = RandomGenerator();
+  }
+  return Events;
+}
+
+BOOST_AUTO_TEST_SUITE(AsciiData);
+
+BOOST_AUTO_TEST_CASE(TestMomentumEnergyOrder) {
+  std::list<std::string> TestFiles{
+      "Data_AsciiReaderTest-CorrectWeightedMomE.dat",
+      "Data_AsciiReaderTest-CorrectWeightedEmom.dat"};
+  for (const auto &FileName : TestFiles) {
+    auto Events = readData(FileName, 10);
+    BOOST_CHECK_EQUAL(Events.size(), 3);
+
+    const auto &Event = Events.front();
+    BOOST_CHECK_EQUAL(Event.Weight, .77);
+
+    auto Particle = Event.ParticleList.at(0);
+    BOOST_CHECK_EQUAL(Particle.pid(), 123);
+    BOOST_CHECK_EQUAL(Particle.fourMomentum().e(), 5.);
+    BOOST_CHECK_EQUAL(Particle.fourMomentum().px(), .543);
+    BOOST_CHECK_EQUAL(Particle.fourMomentum().py(), .2345);
+    BOOST_CHECK_EQUAL(Particle.fourMomentum().pz(), 1.);
+
+    Particle = Event.ParticleList.at(2);
+    BOOST_CHECK_EQUAL(Particle.pid(), -123);
+    BOOST_CHECK_EQUAL(Particle.fourMomentum().e(), 9.);
+    BOOST_CHECK_EQUAL(Particle.fourMomentum().px(), .85434);
+    BOOST_CHECK_EQUAL(Particle.fourMomentum().py(), .564);
+    BOOST_CHECK_EQUAL(Particle.fourMomentum().pz(), .923);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(TestOverwrite) {
+  const char *Filename = "Data_AsciiReaderTest-TestOverwrite.dat";
+  auto Events1 = generateSample(3);
+  writeData(Events1, Filename);
+  auto Events2 = generateSample(4);
+  writeData(Events2, Filename, true);
+  auto ImportedEvents = readData(Filename);
+  BOOST_CHECK_EQUAL(ImportedEvents.size(), Events1.size() + Events2.size());
+  std::remove(Filename);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
+} // namespace Ascii
+} // namespace Data
+} // namespace ComPWA
