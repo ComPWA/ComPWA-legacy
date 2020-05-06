bcaedd1e4119ea731116c137266a9355977f6ae8
Remco de Boer <redeboer@gmx.com>
2020-05-06 11:11:47 +0200

    style: apply black to conf + add requirements-dev

--- doc/conf.py
+++ doc/conf.py
@@ -7,14 +7,16 @@
 # -- Path setup --------------------------------------------------------------
 
 import os
+
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
+
+    html_theme = "sphinx_rtd_theme"
     html_theme_path = [
         sphinx_rtd_theme.get_html_theme_path(),
     ]
@@ -22,9 +24,9 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
 # -- Project information -----------------------------------------------------
 
-project = 'ComPWA'
-copyright = '2020, The ComPWA Team'
-author = 'The ComPWA Team'
+project = "ComPWA"
+copyright = "2020, The ComPWA Team"
+author = "The ComPWA Team"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,32 +35,34 @@ author = 'The ComPWA Team'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'breathe',
-    'exhale',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.intersphinx',
+    "breathe",
+    "exhale",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.intersphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
 
 # -- Extension settings ------------------------------------------------------
 
 # Setup Sphinx
-primary_domain = 'cpp'
-highlight_language = 'cpp'
-html_logo = 'images/logo-small.png'
+primary_domain = "cpp"
+highlight_language = "cpp"
+html_logo = "images/logo-small.png"
 
 # Setup Breathe
-breathe_projects = {
-    "ComPWA": "./doxyoutput/xml"
-}
+breathe_projects = {"ComPWA": "./doxyoutput/xml"}
 breathe_default_project = "ComPWA"
 
 # Setup Exhale
@@ -76,5 +80,5 @@ exhale_args = {
 
 # Setup intersphinx
 intersphinx_mapping = {
-    'pycompwa': ('https://compwa.github.io/', None),
+    "pycompwa": ("https://compwa.github.io/", None),
 }
